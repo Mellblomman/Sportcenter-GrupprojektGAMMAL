cc4bd67a98f3d71fe2cda462f432ed7a365a39ca
Enkas1 <lukas.engdahl@hotmail.com>
2024-01-26 15:23:56 +0100

    la till index.htm och app.py +venv

---
+++ venv/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template, url_for
+import os
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def render_index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
