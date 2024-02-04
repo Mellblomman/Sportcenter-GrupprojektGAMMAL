0ee7889bf8e3c10630ad3adbc3288b054ddd0dd2
Mellblomman <115022366+Mellblomman@users.noreply.github.com>
2024-02-04 11:50:45 +0100

    renamed the admin file

---
+++ admin.sql
@@ -0,0 +1,22 @@
+UPDATE Banor
+SET pris = new_price
+WHERE bana_id = your_bana_id;
+
+
+UPDATE Banor
+SET tillgänglighet = CASE WHEN tillgänglighet = 'True' THEN 'False' ELSE 'True' END
+WHERE bana_id = your_bana_id;
+
+
+
+UPDATE Bokningar
+SET tid_datum = new_time_date
+WHERE boknings_id = your_boknings_id;
+
+
+INSERT INTO Banor (bana_id, Aktivitet, tid, datum, pris, tillgänglighet)
+VALUES (your_bana_id, 'New Activity', 'New Time', 'New Date', new_price, 'Available');
+
+
+DELETE FROM Banor
+WHERE bana_id = your_bana_id;
